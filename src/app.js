a946353a467da65e31c4c01595d3c10cfc68f5b9
Mike Chu <mike@chu.ms>
2017-08-02 14:58:59 -0600

    Confirmed copying from one S3 bucket  to another.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,27 @@
 require("babel-polyfill");
 
+import AWS from 'aws-sdk';
+import config from './config.json'
+import fs from 'fs-extra';
+
 class App {
     static main() {
-        console.log('App is ready for development.');
+        AWS.config.accessKeyId = config.credentials.accessKeyId;
+        AWS.config.secretAccessKey = config.credentials.secretAccessKey;
+
+        const s3 = new AWS.S3();
+
+        s3.copyObject({ 
+            Bucket: config.buckets.destination,
+            CopySource: `${config.buckets.source}/TestFileFromSakai.txt`,
+            Key: `TestFileFromSakaiButRenamed.txt`,
+        }, (error, data) => {
+            if (error) {
+                console.log(error);
+                return;
+            }
+            console.log(data);
+        });
     }
 }
 
