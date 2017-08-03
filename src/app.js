090b8571b9c54051834ba4b2e2762a1be6575b94
Mike Chu <mike@chu.ms>
2017-08-03 12:41:35 -0600

    Reading CSV line-by-line and executing copyObject at S3

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ require("babel-polyfill");
 import AWS from 'aws-sdk';
 import config from './config.json'
 import fs from 'fs-extra';
+import readline from 'readline';
 
 class App {
     static main() {
@@ -11,18 +12,38 @@ class App {
 
         const s3 = new AWS.S3();
 
-        s3.copyObject({ 
-            Bucket: config.buckets.destination,
-            CopySource: `${config.buckets.source}/TestFileFromSakai.txt`,
-            Key: `TestFileFromSakaiButRenamed.txt`,
-        }, (error, data) => {
-            if (error) {
-                console.log(error);
-                return;
-            }
-            console.log(data);
+        const lineReader = readline.createInterface({
+            input: fs.createReadStream(`${config.workingPath}\\files.csv`)
+        });
+
+        const header = [];
+        lineReader.once('line', (dataline) => {
+            const line = dataline.replace(new RegExp('"', 'g'), '');
+            header.push(...line.split(','));
+        });
+
+        lineReader.on('line', (dataline) => {
+            const line = dataline.replace(new RegExp('"', 'g'), '');
+            const file = {};
+
+            const data = line.split(',');
+            header.map((fieldName, index) => {
+                file[fieldName] = data[index];
+            });
+
+            s3.copyObject({
+                Bucket: config.buckets.destination,
+                CopySource: `${config.buckets.source}/${file.filePathOnDisk}`,
+                Key: `${file.targetFileName}`,
+            }, (error, data) => {
+                if (error) {
+                    console.log(error);
+                    return;
+                }
+                console.log(data);
+            });
         });
     }
 }
 
-App.main();
+App.main(); 
